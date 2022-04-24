e829461bd0ea862046464cd4a07fc476595c4b03
Santiago Jaramillo López <santiagojaralopez@gmail.com>
2022-04-24 14:18:15 -0500

    I could say minesweeper project is finished jeje

--- corte1/buscaminas_sparsematrix/main.py
+++ corte1/buscaminas_sparsematrix/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from scipy.sparse import csr_matrix
 
 
@@ -6,14 +7,18 @@ def create_mines_map(matrix):
     mines_map = []
     for row in matrix:
         mines_map.append(['#' for _ in row])
-    return mines_map
+    return np.array(mines_map)
 
 
 def print_matrix(matrix):
     for row in matrix:
-        for _ in row:
-            print('#', end=' ')
+        for i in row:
+            print(i, end=' ')
         print('')
+
+
+def clear():
+    os.system('clear')
 
 
 def run():
@@ -30,23 +35,28 @@ def run():
 
     steps = []
     sparse_matrix = csr_matrix(matrix)
-    mines_map = np.array(create_mines_map(matrix), dtype=str)
+    mines_map = create_mines_map(matrix)
     flag = False
 
     while not flag:
+        clear()
         print_matrix(mines_map)
 
         row = int(input('Numero de la fila a seleccionar: '))
         column = int(input('Numero de la columna a seleccionar: '))
 
-        try:
-            if matrix[row, column] in sparse_matrix.data:
-                print('Mina!!\nGAME OVER')
-                flag = True
-            else:
-                mines_map[row, column] = '*'
-        except IndexError:
-            print('Coordenada no existente, intenta con otra')
+        if (row, column) in steps:
+            print('Ya pasaste por aquí, elige otra casilla')
+        else:
+            try:
+                if matrix[row, column] in sparse_matrix.data:
+                    print('Pisaste una mina!!\nGAME OVER')
+                    flag = True
+                else:
+                    mines_map[row, column] = '*'
+                    steps.append((row, column))
+            except IndexError:
+                print('Coordenada no existente, intenta con otra')
 
 
 if __name__ == '__main__':
