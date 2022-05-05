9974765c6a8f55b29f6f554d86862517684b7a29
Maria Fernanda Lopez <85798782+MariaFernandaL@users.noreply.github.com>
2022-05-05 15:03:14 -0500

    commit parcial

--- co.edu.cue.ed.parcial1/src/co/edu/cue/ed/parcial1/app4/MainTurn.java
+++ co.edu.cue.ed.parcial1/src/co/edu/cue/ed/parcial1/app4/MainTurn.java
@@ -1,44 +1,41 @@
 package co.edu.cue.ed.parcial1.app4;
 
-import java.rmi.server.UID;
 
 import javax.swing.JOptionPane;
-import javax.swing.text.Segment;
-import co.edu.cue.ed.parcial1.model4.SegmentType;
-import co.edu.cue.ed.parcial1.model4.Terminal;
 import co.edu.cue.ed.parcial1.model4.Turn;
-import co.edu.cue.ed.parcial1.model4.User;
-import javafx.print.PrinterJob.JobStatus;
 
 public class MainTurn {
 
 	public static void main(String[] args) {
 		
 		int option;	
-		Terminal terminal= new Terminal();
-		Turn turn= new Turn();
+		Turn turn = new Turn();
 		
 		do {
 			option=Integer.parseInt(JOptionPane.showInputDialog("Enter the option for the menu: "
 					+ " \n 1. Create user"
 					+ " \n 2. Show users"
-					+ " \n 3. Show ticket"
-					+ " \n 4. OUT"
+					+ " \n 3. Show counter of students and teachers"
+					+ " \n 4. Show ticket"
+					+ " \n 5. OUT"
 					));
 			
 			switch (option) {
 			case 1:
-				Terminal.createUsers();
+				turn.createUsers();
 				break;
 			case 2:
-				System.out.println(turn.getUsersList().toString());
+				turn.showUsers();
+				break;
+			case 3:
+				turn.showCounters();
 				break;
 			case 4:
 				break;
 			default:
 				break;
 			}
-		} while (option!=3);
+		} while (option!=5);
 			
 		}
 	
