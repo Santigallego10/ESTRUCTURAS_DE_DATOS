f1d6452b3236a21750ee30fe90c601f4291dd329
Santigallego10 <90339673+Santigallego10@users.noreply.github.com>
2022-05-09 12:20:19 -0500

    EJEMPLO 2

---
+++ Genericos/src/Main/Ejemplo2/EjemploGenericos2.java
@@ -0,0 +1,28 @@
+package Main.Ejemplo2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EjemploGenericos2 {
+    public static void main(String[] args) {
+        Product [] productsArray = {new Product(1015,"CellPhone",200),
+                new Product(1013,"Tshirt",20)};
+        Integer[] offerValues = {1, 5, 8};
+        List<Product> productList = fromArrayToList(productsArray);
+        List<Integer> integerList = fromArrayToList(offerValues);
+        productList.forEach(System.out::println);
+
+        List<String> usersNames = fromArrayToList(new String[]{"Pepe","Mafe"},
+                offerValues);
+
+    }
+    private static <T>List<T> fromArrayToList(T[] c) {
+        return Arrays.asList(c);
+    }
+    private static <T extends String, G>List<T> fromArrayToList(T[] c, G[] x) {
+        for (G element:x) {
+            System.out.println(element);
+        }
+        return Arrays.asList(c);
+    }
+}
---
+++ Genericos/src/Main/Ejemplo2/Product.java
@@ -0,0 +1,46 @@
+package Main.Ejemplo2;
+
+public class Product {
+    private int cod;
+    private String name;
+    private float sellValue;
+
+    public Product(int cod, String name, float sellValue) {
+        this.cod = cod;
+        this.name = name;
+        this.sellValue = sellValue;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSellValue() {
+        return sellValue;
+    }
+
+    public void setSellValue(float sellValue) {
+        this.sellValue = sellValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "cod=" + cod +
+                ", name='" + name + '\'' +
+                ", sellValue=" + sellValue +
+                '}';
+    }
+}
