9bac0aa4c5c09c306a11d9ec26138c58790b7309
Santigallego10 <90339673+Santigallego10@users.noreply.github.com>
2022-05-15 12:20:16 -0500

    test location

--- untitled/src/application/MainApplication.java
+++ untitled/src/application/MainApplication.java
@@ -1,9 +1,17 @@
 package application;
 
-import application.Entities.BolsaSupermercado;
-import application.Entities.Cleaning;
+import application.Entities.*;
 
 public class MainApplication {
 
-    BolsaSupermercado<Cleaning> productosLimpieza = new BolsaSupermercado<>();
+    BolsaSupermercado<Cleaning> cleaningProducts = new BolsaSupermercado<>();
+    BolsaSupermercado<Dairy> dairyProducts = new BolsaSupermercado<>();
+    BolsaSupermercado<Fruit> fruitProducts = new BolsaSupermercado<>();
+    BolsaSupermercado<Nonperishable> nonperishableProducts = new BolsaSupermercado<>();
+
+    
+
+
+
+
 }
